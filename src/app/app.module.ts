635f30c0e6f7676c000bcaec8538c70287e49f7d
momotsow <kgomotsomkansi@gmail.com>
2019-10-14 12:08:57 +0200

    added a login and signup component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './todo/todo.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoComponent
+    TodoComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
